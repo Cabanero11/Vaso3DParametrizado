0d5eb969e631dcae73d6c45b9ebb847c6926eebe
Cabanero11 <cabanero1111@gmail.com>
2024-04-17 21:47:48 +0200

    Convertido de JAVA a C#
    y posible ejecucion

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,58 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxR1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBoxR2 = new System.Windows.Forms.TextBox();
+            this.textBoxH1 = new System.Windows.Forms.TextBox();
+            this.textBoxH2 = new System.Windows.Forms.TextBox();
+            this.textBoxW1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // textBoxR1
             // 
-            this.textBox1.Location = new System.Drawing.Point(293, 111);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 0;
+            resources.ApplyResources(this.textBoxR1, "textBoxR1");
+            this.textBoxR1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxR1.Name = "textBoxR1";
             // 
             // button1
             // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Location = new System.Drawing.Point(296, 153);
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(97, 49);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Generar Obj";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.BackColor = System.Drawing.Color.DarkTurquoise;
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.Location = new System.Drawing.Point(296, 220);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(97, 49);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Abrir carpeta del .obj";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxR2
+            // 
+            resources.ApplyResources(this.textBoxR2, "textBoxR2");
+            this.textBoxR2.Name = "textBoxR2";
+            // 
+            // textBoxH1
+            // 
+            resources.ApplyResources(this.textBoxH1, "textBoxH1");
+            this.textBoxH1.Name = "textBoxH1";
+            // 
+            // textBoxH2
+            // 
+            resources.ApplyResources(this.textBoxH2, "textBoxH2");
+            this.textBoxH2.Name = "textBoxH2";
+            // 
+            // textBoxW1
+            // 
+            resources.ApplyResources(this.textBoxW1, "textBoxW1");
+            this.textBoxW1.Name = "textBoxW1";
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Name = "panel1";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // panel2
+            // 
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Name = "panel2";
+            // 
+            // panel3
+            // 
+            resources.ApplyResources(this.panel3, "panel3");
+            this.panel3.Controls.Add(this.textBoxR1);
+            this.panel3.Controls.Add(this.textBoxR2);
+            this.panel3.Controls.Add(this.textBoxH1);
+            this.panel3.Controls.Add(this.textBoxW1);
+            this.panel3.Controls.Add(this.textBoxH2);
+            this.panel3.Name = "panel3";
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.Parametros;
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AcceptButton = this.button1;
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(720, 480);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
-            this.Text = "Vaso Parametrizado 3D";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxR1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxR2;
+        private System.Windows.Forms.TextBox textBoxH1;
+        private System.Windows.Forms.TextBox textBoxH2;
+        private System.Windows.Forms.TextBox textBoxW1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,15 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Diagnostics;
 using System.IO;
-using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Windows.Forms;
 using System.Globalization;
 
 namespace WindowsFormsApp1
@@ -19,7 +12,6 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
-            button1.Click += btnGenerar_Click;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -27,108 +19,53 @@ namespace WindowsFormsApp1
 
         }
 
-        // EVENTO DE LLAMAR AL BOTON
-        private void btnGenerar_Click(object sender, EventArgs e)
+
+        // TRANSFORMACION DEL CODIGO DEL DE JAVA AAAA
+        private void FuncionGenerarCilindro(int numLados, double altura, double radio, string nombreArchivo)
         {
-            int numFaces = 20;
-            double sideLength = 1.0;
-            double height = 2.0;
-            double[] position = new double[] { 0, 0, 0 };
-            double[] orientation = new double[] { 0, 0, 0 };
+            List<List<double>> vertices = new List<List<double>>();
+            List<List<int>> caras = new List<List<int>>();
 
-            string exeFolder = AppDomain.CurrentDomain.BaseDirectory;
-            string outputPath = Path.Combine(exeFolder, "vaso.obj");
+            // Crear vértices
+            for (int i = 0; i < numLados; i++)
+            {
+                double angulo = 2 * Math.PI * i / numLados;
+                double x = radio * Math.Cos(angulo);
+                double y = radio * Math.Sin(angulo);
+                vertices.Add(new List<double>() { x, y, 0 }); // Parte inferior del cilindro
+                vertices.Add(new List<double>() { x, y, altura }); // Parte superior del cilindro
+            }
 
-            GeneratePolygonalPrismObjFile(numFaces, sideLength, height, position, orientation, outputPath);
+            // Calcular caras
+            for (int i = 0; i < numLados; i++)
+            {
+                caras.Add(new List<int>() { i * 2, (i * 2 + 2) % (numLados * 2), (i * 2 + 4) % (numLados * 2), i * 2 + 4 });
+                caras.Add(new List<int>() { i * 2 + 1, (i * 2 + 5) % (numLados * 2), (i * 2 + 3) % (numLados * 2), (i * 2 + 1) % (numLados * 2) });
+                caras.Add(new List<int>() { i * 2, i * 2 + 1, (i * 2 + 1) % (numLados * 2), (i * 2 + 2) % (numLados * 2) });
+                caras.Add(new List<int>() { (i * 2 + 2) % (numLados * 2), i * 2 + 4, (i * 2 + 3) % (numLados * 2), (i * 2 + 5) % (numLados * 2) });
+            }
 
-            MessageBox.Show("¡El archivo se ha generado y guardado exitosamente en la carpeta de la aplicación!", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            // Guardar los vértices y caras en un archivo .obj
+            GuardarObjetoObj(vertices, caras, nombreArchivo);
         }
 
-
-        public static void GeneratePolygonalPrismObjFile(int numFaces, double sideLength, double height, double[] position, double[] orientation, string outputFileName)
+        private void GuardarObjetoObj(List<List<double>> vertices, List<List<int>> caras, string nombreArchivo)
         {
-            double[][] baseVertices = new double[numFaces][];
-            double angleIncrement = 2 * Math.PI / numFaces;
-
-            for (int i = 0; i < numFaces; i++)
-            {
-                double x = sideLength / 2 * Math.Cos(i * angleIncrement);
-                double y = sideLength / 2 * Math.Sin(i * angleIncrement);
-                baseVertices[i] = new double[] { x, y, -height / 2 };
-            }
-
-            // CASOS DE NUMFACES
-            if (numFaces == 7)
-            {
-                Console.WriteLine("Solo se admite un Icosagon para este ejemplo.");
-                return;
-            }
-            else if (numFaces == 20)
-            {
-                baseVertices = new double[20][];
-                for (int i = 0; i < numFaces; i++)
-                {
-                    double x = sideLength / 2 * Math.Cos(i * angleIncrement);
-                    double y = sideLength / 2 * Math.Sin(i * angleIncrement);
-                    baseVertices[i] = new double[] { x, y, -height / 2 };
-                }
-            }
-            else
-            {
-                Console.WriteLine("Le pepe");
-                return;
-            }
-
-            double[][] allVertices = baseVertices;
-
-            // LAS CARAS SE GUARDAN ACA
-            int[][] faces = GenerateFaces(numFaces);
-
-            double[][] faceNormals = new double[faces.Length][];
-            for (int i = 0; i < faces.Length; i++)
-            {
-                int[] face = faces[i];
-                double[] v1 = allVertices[face[0]];
-                double[] v2 = allVertices[face[1]];
-                double[] v3 = allVertices[face[2]];
-                double[] normal = CalculateNormal(v1, v2, v3);
-                faceNormals[i] = normal;
-            }
-
             try
             {
-                using (StreamWriter writer = new StreamWriter(outputFileName))
+                CultureInfo culture = new CultureInfo("en-US"); // Establecer cultura en-US para formatear números decimales con puntos
+                using (StreamWriter writer = new StreamWriter(nombreArchivo))
                 {
-                    foreach (double[] vertex in allVertices)
+                    foreach (List<double> vertex in vertices)
                     {
-                        writer.Write("v ");
-                        foreach (double coordinate in vertex)
-                        {
-                            string coordinateString = coordinate.ToString("F2", CultureInfo.InvariantCulture).Replace(',', '.');
-                            writer.Write($"{coordinateString} ");
-                        }
-                        writer.WriteLine();
+                        string formattedVertex = $"{vertex[0]:F2} {vertex[1]:F2} {vertex[2]:F2}"; // Formatear vértice
+                        formattedVertex = formattedVertex.Replace(',', '.'); // Reemplazar comas por puntos
+                        writer.WriteLine($"v {formattedVertex}"); // Escribir vértice en el archivo
                     }
 
-                    foreach (double[] normal in faceNormals)
+                    foreach (List<int> face in caras)
                     {
-                        writer.Write("vn ");
-                        foreach (double coordinate in normal)
-                        {
-                            string coordinateString = coordinate.ToString("F2", CultureInfo.InvariantCulture).Replace(',', '.');
-                            writer.Write($"{coordinateString} ");
-                        }
-                        writer.WriteLine();
-                    }
-
-                    foreach (int[] face in faces)
-                    {
-                        writer.Write("f ");
-                        foreach (int vertexIndex in face)
-                        {
-                            writer.Write($"{vertexIndex}//{vertexIndex} ");
-                        }
-                        writer.WriteLine();
+                        writer.WriteLine($"f {face[0] + 1}//{face[0] + 1} {face[1] + 1}//{face[1] + 1} {face[2] + 1}//{face[2] + 1} {face[3] + 1}//{face[3] + 1}"); // Escribir cara en el archivo
                     }
                 }
             }
@@ -138,107 +75,18 @@ namespace WindowsFormsApp1
             }
         }
 
-        private static double[] CalculateNormal(double[] v1, double[] v2, double[] v3)
+
+        private void btnGenerar_Click(object sender, EventArgs e)
         {
-            double[] u = { v2[0] - v1[0], v2[1] - v1[1], v2[2] - v1[2] };
-            double[] v = { v3[0] - v1[0], v3[1] - v1[1], v3[2] - v1[2] };
-            double[] normal = new double[3];
-            normal[0] = u[1] * v[2] - u[2] * v[1];
-            normal[1] = u[2] * v[0] - u[0] * v[2];
-            normal[2] = u[0] * v[1] - u[1] * v[0];
-            double length = Math.Sqrt(normal[0] * normal[0] + normal[1] * normal[1] + normal[2] * normal[2]);
-            normal[0] /= length;
-            normal[1] /= length;
-            normal[2] /= length;
-            return normal;
-        }
+            // Obtener los parámetros del usuario
+            int numLados = int.Parse(textBoxH1.Text);
+            double altura = double.Parse(textBoxH2.Text);
+            double radio = double.Parse(textBoxR1.Text);
 
-        private static double[] RotateVertex(double[] vertex, double[] orientation)
-        {
-            double x = vertex[0] * (Math.Cos(orientation[1]) * Math.Cos(orientation[2])) - vertex[1] * (Math.Cos(orientation[1]) * Math.Sin(orientation[2])) + vertex[2] * Math.Sin(orientation[1]);
-            double y = vertex[0] * (Math.Cos(orientation[0]) * Math.Sin(orientation[2]) + Math.Sin(orientation[0]) * Math.Sin(orientation[1]) * Math.Cos(orientation[2])) + vertex[1] * (Math.Cos(orientation[0]) * Math.Cos(orientation[2]) - Math.Sin(orientation[0]) * Math.Sin(orientation[1]) * Math.Sin(orientation[2])) - vertex[2] * Math.Sin(orientation[0]) * Math.Cos(orientation[1]);
-            double z = vertex[0] * (Math.Sin(orientation[0]) * Math.Sin(orientation[2]) - Math.Cos(orientation[0]) * Math.Sin(orientation[1]) * Math.Cos(orientation[2])) + vertex[1] * (Math.Sin(orientation[0]) * Math.Cos(orientation[2]) + Math.Cos(orientation[0]) * Math.Sin(orientation[1]) * Math.Sin(orientation[2])) + vertex[2] * Math.Cos(orientation[0]) * Math.Cos(orientation[1]);
-            return new double[] { x, y, z };
-        }
+            // Generar el cilindro con los parámetros especificados
+            FuncionGenerarCilindro(numLados, altura, radio, "cilindro.obj");
 
-        private static double[][] RotatePolygonalPrism(double[][] vertices, double[] orientation)
-        {
-            double[][] rotatedVertices = new double[vertices.Length][];
-            for (int i = 0; i < vertices.Length; i++)
-            {
-                rotatedVertices[i] = RotateVertex(vertices[i], orientation);
-            }
-            return rotatedVertices;
-        }
-
-        public static int[][] GenerateFaces(int numFaces)
-        {
-            switch (numFaces)
-            {
-                case 7:
-                    return new int[][]
-                    {
-                        // First base heptagon
-                        new int[] {1, 2, 8}, new int[] {2, 3, 8}, new int[] {3, 4, 8}, new int[] {4, 5, 8}, new int[] {5, 6, 8}, new int[] {6, 7, 8}, new int[] {7, 1, 8},
-                        // Second base heptagon
-                        new int[] {9, 10, 16}, new int[] {10, 11, 16}, new int[] {11, 12, 16}, new int[] {12, 13, 16}, new int[] {13, 14, 16}, new int[] {14, 15, 16}, new int[] {15, 9, 16},
-                        // Side triangles
-                        new int[] {1, 2, 9}, new int[] {2, 3, 10}, new int[] {3, 4, 11}, new int[] {4, 5, 12}, new int[] {5, 6, 13}, new int[] {6, 7, 14}, new int[] {7, 1, 15},
-                        // Additional side triangles to complete the rectangle
-                        new int[] {9, 10, 2}, new int[] {10, 11, 3}, new int[] {11, 12, 4}, new int[] {12, 13, 5}, new int[] {13, 14, 6}, new int[] {14, 15, 7}, new int[] {15, 9, 1}
-                    };
-                case 20:
-                    return new int[][]
-            {
-                // Bottom face
-                new int[] { 0, 1, 2, 3, 4 },
-                new int[] { 1, 2, 3, 4, 5 },
-                new int[] { 2, 3, 4, 5, 6 },
-                new int[] { 3, 4, 5, 6, 7 },
-                new int[] { 4, 5, 6, 7, 8 },
-                new int[] { 5, 6, 7, 8, 9 },
-                new int[] { 6, 7, 8, 9, 10 },
-                new int[] { 7, 8, 9, 10, 11 },
-                new int[] { 8, 9, 10, 11, 12 },
-                new int[] { 9, 10, 11, 12, 13 },
-                new int[] { 10, 11, 12, 13, 14 },
-                new int[] { 11, 12, 13, 14, 15 },
-                new int[] { 12, 13, 14, 15, 16 },
-                new int[] { 13, 14, 15, 16, 17 },
-                new int[] { 14, 15, 16, 17, 18 },
-                new int[] { 15, 16, 17, 18, 19 },
-                new int[] { 16, 17, 18, 19, 0 },
-                new int[] { 17, 18, 19, 0, 1 },
-                new int[] { 18, 19, 0, 1, 2 },
-                new int[] { 19, 0, 1, 2, 3 },
-
-                // Sides
-                new int[] { 0, 1, numFaces + 1 },
-                new int[] { 1, 2, numFaces + 2 },
-                new int[] { 2, 3, numFaces + 3 },
-                new int[] { 3, 4, numFaces + 4 },
-                new int[] { 4, 5, numFaces + 5 },
-                new int[] { 5, 6, numFaces + 6 },
-                new int[] { 6, 7, numFaces + 7 },
-                new int[] { 7, 8, numFaces + 8 },
-                new int[] { 8, 9, numFaces + 9 },
-                new int[] { 9, 10, numFaces + 10 },
-                new int[] { 10, 11, numFaces + 11 },
-                new int[] { 11, 12, numFaces + 12 },
-                new int[] { 12, 13, numFaces + 13 },
-                new int[] { 13, 14, numFaces + 14 },
-                new int[] { 14, 15, numFaces + 15 },
-                new int[] { 15, 16, numFaces + 16 },
-                new int[] { 16, 17, numFaces + 17 },
-                new int[] { 17, 18, numFaces + 18 },
-                new int[] { 18, 19, numFaces + 19 },
-                new int[] { 19, 0, numFaces },
-            };
-
-                default:
-                    Console.WriteLine("Le pepe");
-                    return null;
-            }
+            MessageBox.Show("Archivo cilindro.obj creado con éxito!", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -246,5 +94,20 @@ namespace WindowsFormsApp1
             string currentDirectory = Environment.CurrentDirectory;
             Process.Start(currentDirectory);
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Valores predeterminados para los parámetros del cilindro
+            int numLadosDefault = 20;
+            double alturaDefault = 10.0;
+            double radioDefault = 5.0;
+
+            // Generar el cilindro con los valores predeterminados
+            FuncionGenerarCilindro(numLadosDefault, alturaDefault, radioDefault, "cilindro_default.obj");
+
+            MessageBox.Show("Archivo cilindro_default.obj creado con éxito!", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
     }
+
+   
 }
